3b98a87815b69ac44904ad84b5f1cf77b3c7d3c5
Karunesh <karunesh.gurkarnme@cerner.com>
2020-04-04 19:31:57 +0530

    Pushing to git

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,16 @@
-import React from 'react';
-import { Link } from 'gatsby';
+import React from "react";
+import { Link } from "gatsby";
 
-import Container from 'components/Container';
+import Container from "components/Container";
 
 const Header = () => {
   return (
     <header>
       <Container type="content">
-        <p>My Gatsby Site</p>
+        <p>Corana Cases Count</p>
         <ul>
           <li>
             <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2/">Page 2</Link>
           </li>
         </ul>
       </Container>
